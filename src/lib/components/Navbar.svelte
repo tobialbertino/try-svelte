fa190eb8550a6854354124165c67cf3a2a8a87ae
Tobi Albertino <muhamad_tobiaja232@apps.ipb.ac.id>
2022-12-02 00:40:53 +0700

    simplified structure

--- src/lib/components/Navbar.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-</script>
-
-<nav>
-    <a href="/#">Root</a>
-    <a href="/home">Home</a>
-    <a href="/#">#</a>
-</nav>
--- src/routes/home/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { Navbar, Header, Footer } from "$lib/index";
-    let name: String = "Tobi";
-</script>
-
-<Header />
-<Navbar />
-<h1>Hello {name}</h1>
-<Footer />
