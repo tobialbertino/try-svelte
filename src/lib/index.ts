80fe21a359994f58cdbefa614ca80e48311af8a1
Tobi Albertino <muhamad_tobiaja232@apps.ipb.ac.id>
2022-12-18 17:03:20 +0700

    reset, try ver 1 sveltekit

--- src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-import Navbar from '$lib/components/Navbar.svelte'
-import Header from '$lib/components/Header.svelte'
-import Footer from '$lib/components/Footer.svelte'
-
-export { Navbar, Header, Footer }
