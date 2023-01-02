2a3c57fb8c33232aa4ba79ddd2a7285eb4d288d6
Tobi Albertino <muhamad_tobiaja232@apps.ipb.ac.id>
2023-01-02 13:42:39 +0700

    episode-4

--- src/routes/(try)/try/+page.svelte
+++ src/routes/(try)/try/+page.svelte
@@ -1,7 +1,41 @@
 <script lang="ts">
-
 </script>
 
-<h1 class="text-3xl font-bold underline">
-    Hello world! -> try tailwnd
-</h1>
+<svelte:head>
+	<title>Learn TailwindCSS</title>
+</svelte:head>
+
+<h1 class="text-8xl font-bold text-center my-10">Mari Belajar <span class="bg-gradient-to-r from-blue-500 to-teal-500 bg-clip-text text-transparent">TailwindCSS</span></h1>
+
+<section class="mb-10 bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-200">
+	<h2 class="text-3xl text-center">Spacing</h2>
+	<div class="m-4 bg-red-200">Margin</div>
+	<div class="py-8 bg-red-200">Padding</div>
+</section>
+
+<section class="mb-10 bg-green-200">
+	<h2 class="text-3xl text-center">Sizing</h2>
+	<div class="w-32 h-32 bg-yellow-200">Kotak 1</div>
+</section>
+
+<section class="mb-96 font-sans max-w-3xl mx-auto ">
+	<h2 class="mb-2 text-4xl font-bold">Belajar Mengenai Typography pada TailwindCSS</h2>
+	<div class="mb-4 text-lg text-gray-500">By <a href="/" class="text-gray-700 font-semibold">Tobi Albertino</a>, 1 Januari 2023</div>
+    <p class="mb-4">
+        Lorem ipsum dolor, sit amet <a href="/" class="font-semibold underline text-blue-600">consectetur</a> adipisicing elit. Molestiae, fugit suscipit. Rerum sequi deserunt autem modi quidem, ex quisquam nobis aut, sed, iure dignissimos facilis. Accusantium similique tempore corrupti voluptatem, consequatur laboriosam, officiis neque necessitatibus earum assumenda aspernatur, doloremque eos at ullam minus! Ea iste explicabo doloremque? Quaerat, maxime eum.
+    </p>
+    <blockquote class="mb-4 italic font-thin text-xl">
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti laborum aut maiores ea corporis nihil laudantium dignissimos nemo quas et!""
+    </blockquote>
+    <p class="mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet esse laborum corporis nihil, minus unde sapiente. Nobis voluptates blanditiis ad, doloremque natus minus suscipit voluptas eligendi maiores perferendis repellat iste.
+    </p>
+</section>
+
+
+
+<style>
+	* {
+		border: 1px solid red;
+	}
+</style>
